76604fe3b41f4db3367423b64b5da8f8b61391c8
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-31 12:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target')
 }
+
+function increaseRankBy(n) {
+  const lis = document
+  .querySelectorAll('ul.ranked-list');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).parseInt();
+  }
+}
